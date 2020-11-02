ddfbc689d1b43a9fce036507d0301f3212b16a1f
张少强 <shaoqiang_zhang@icloud.com>
2020-11-02 15:00:01 +0800

    引入ant-design-vue组件库

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import Element from 'element-ui'
+import "ant-design-vue/dist/antd.css";
 import "element-ui/packages/theme-chalk/src/reset.scss";
 import "element-ui/packages/theme-chalk/src/index.scss";
 
+Vue.use(Antd)
 Vue.use(Element)
